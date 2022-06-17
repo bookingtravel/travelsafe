2a398953b5bc66e61fc505f1f18cf46d99578d25
Samashti Gautam <81379609+samashtiii@users.noreply.github.com>
2022-06-17 17:29:06 +0530

    Add files via upload

--- home.php
+++ home.php
@@ -6,7 +6,7 @@
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>HOME</title>
 <!-- swiper css link  -->
-<link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css"/>
+<link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css"/>
 <!-- font cdn link -->
 <link rel="stylesheet"  href=https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css>>
 
@@ -31,6 +31,154 @@
 
 
 </section>
+
+
+<!--service section -->
+<section class="services">
+
+   <h1 class="heading-title"> Services we provide </h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <img src="css\images\icon-1.png" alt="">
+         <h3>adventure</h3>
+      </div>
+
+      <div class="box">
+         <img src="css\images\icon-2.png" alt="">
+         <h3>tour guide</h3>
+      </div>
+
+      <div class="box">
+         <img src="css\images\icon-3.png" alt="">
+         <h3>trekking</h3>
+      </div>
+
+      <div class="box">
+         <img src="css\images\icon-4.png" alt="">
+         <h3>camp fire</h3>
+      </div>
+
+      <div class="box">
+         <img src="css\images\icon-5.png" alt="">
+         <h3>off road</h3>
+      </div>
+
+      <div class="box">
+         <img src="css\images\icon-6.png" alt="">
+         <h3>camping</h3>
+      </div>
+
+   </div>
+
+</section>
+
+<!--homesection start-->
+<section class="home">
+
+   <div class="swiper home-slider">
+
+      <div class="swiper-wrapper">
+
+         <div class="swiper-slide slide" style="background:url(css/images/homeslider-3.jpg) no-repeat">
+            <div class="content">
+               <span>explore, discover, travel</span>
+               <h3>travel arround the world</h3>
+               <a href="package.php" class="btn">discover more</a>
+            </div>
+         </div>
+
+         <div class="swiper-slide slide" style="background:url(css/images/homeslider_2.jpg) no-repeat">
+            <div class="content">
+               <span>explore, discover, travel</span>
+               <h3>discover the new places</h3>
+               <a href="package.php" class="btn">discover more</a>
+            </div>
+         </div>
+
+         <div class="swiper-slide slide" style="background:url(css/images/home-slide-1.jpg) no-repeat">
+            <div class="content">
+               <span>explore, discover, travel</span>
+               <h3>make your tour worthwhile</h3>
+               <a href="package.php" class="btn">discover more</a>
+            </div>
+         </div>
+         
+      </div>
+
+      <div class="swiper-button-next"></div>
+      <div class="swiper-button-prev"></div>
+
+   </div>
+
+</section>
+<!--home about section-->
+<section class="home-about">
+
+   <div class="image">
+      <img src="css\images\about-img.jpg" alt="">
+   </div>
+
+   <div class="content">
+      <h3>about us</h3>
+      <p>"Everything is forseen, yet freedom of choice is granted" And we are here to give you the best choices, from perfect packages to ease your travel to beautiful tour suggestions at the lowest prices, we got you! connect with us, leave your suggestions and enjoy,
+         Bon Voyage!
+      </p>
+      <a href="about.php" class="btn">read more</a>
+   </div>
+
+</section>
+<!--homepackage starts-->
+<section class="home-packages">
+
+   <h1 class="heading-title"> our packages </h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <div class="image">
+            <img src="css/images/img 1 .jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Heavenly Himachal</h3>
+            <p>love, breathe, explore!</p>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="css/images/img-2 .jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Cordelia Cruises</h3>
+            <p>who doesn't want to spend night at cruise? definitely not you, so book now!</p>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+      
+      <div class="box">
+         <div class="image">
+            <img src="css/images/img-3 .jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Simply Kerala</h3>
+            <p>southern heaven!
+               Come for the food, stay for the view!
+             explore beauty of kerala at the lowest prices,         avail 15%off right now!
+            </p>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+   </div>
+
+   <div class="load-more"> <a href="package.php" class="btn">load more</a> </div>
+
+</section>
+
+
 
 
 
